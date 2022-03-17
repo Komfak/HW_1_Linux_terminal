490ddda85b5ef9be315a72c6bdfe46d30713329a
Komfak <komfak1988@gmail.com>
2022-03-17 13:06:39 +0400

    linux_terminal

---
+++ myscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ..
+cd lesson_3/
+mkdir lesson_3_1 lesson_3_2 lesson_3_3
+cd lesson_3_1
+touch 3_1.txt 3_2.txt 3_3.txt 3_1.json 3_2.json
+mkdir lesson_3_1_1 lesson_3_1_2 lesson_3_1_3
+ls
+mv 3_1.txt 3_1.json lesson_3_1_1
+cd lesson_3_1_1/
+ls
